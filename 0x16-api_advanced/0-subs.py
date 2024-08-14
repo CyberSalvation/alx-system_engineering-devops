833c87b3cb1b458832b730329cb7bc7d05e31f1f
CyberSalvation <salvation.tobela@gmail.com>
2024-08-14 03:33:45 +0000

    lets see

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,9 +3,11 @@
 import requests
 def number_of_subscribers(subreddit):
 	"""Return the number of subscribers for a given subreddit."""
-	url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-	headers = {'User-Agent': 'Mozilla/5.0'}
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {'User-Agent': 'Mozilla/5.0 (compatible; my-reddit-app/0.1)'}
+	
 	response = requests.get(url, headers=headers)
+
 	if response.status_code == 200:
 		data = response.json().get('data', {})
 		return data.get('subscribers', 0)
