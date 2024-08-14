9600b0362134a76ae6d0b6b9999a424e7c1d3498
CyberSalvation <salvation.tobela@gmail.com>
2024-08-14 04:46:55 +0000

    tired

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,22 @@
 """Module for querying the number of subscribers in a subreddit."""
 import requests
 from requests.auth import HTTPBasicAuth
+
+
 def number_of_subscribers(subreddit):
 	"""Return the number of subscribers for a given subreddit."""
 	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {'User-Agent': 'Mozilla/5.0 (compatible; my-reddit-app/0.1)',}
-	auth = HTTPBasicAuth('ycTHXTfH2HAhdAUxZ9b3lg','Tc2-DwTGIQ5KrwByvgFOuG7L5ddxJw')
-	
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (compatible; my-reddit-app/0.1)',
+	}
+	auth = HTTPBasicAuth(
+		'ycTHXTfH2HAhdAUxZ9b3lg', 'Tc2-DwTGIQ5KrwByvgFOuG7L5ddxJw'
+	)
+
 	response = requests.get(url, headers=headers, auth=auth)
 
 	if response.status_code == 200:
-		data = response.json().get('data', {})
+	data = response.json().get('data', {})
 		return data.get('subscribers', 0)
 	return 0
+
