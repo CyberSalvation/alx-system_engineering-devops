d838711021d64a08a87e5d7224e45d91d04cd91a
CyberSalvation <salvation.tobela@gmail.com>
2024-08-14 01:51:09 +0000

    y

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""Module for querying the number of subscribers in a subreddit."""
 import requests
 def number_of_subscribers(subreddit):
 	"""Return the number of subscribers for a given subreddit."""
 	url = f"https://www.reddit.com/r/{subreddit}/about.json"
 	headers = {'User-Agent': 'Mozilla/5.0'}
-
 	response = requests.get(url, headers=headers)
-
 	if response.status_code == 200:
 		data = response.json().get('data', {})
 		return data.get('subscribers', 0)
